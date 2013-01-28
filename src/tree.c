79b6b032cec3b8954049a034778a1cde7e6a984e
John Wiegley <johnw@fpcomplete.com>
2013-01-28 16:58:32 -0600

    Count number of non-removed entries in the builder

--- src/tree.c
+++ src/tree.c
@@ -233,8 +233,15 @@ unsigned int git_tree_entrycount(git_tree *tree)
 
 unsigned int git_treebuilder_entrycount(git_treebuilder *bld)
 {
+        int count = 0;
+        int i;
 	assert(bld);
-	return bld->entries.length;
+	for (i = 0; i < bld->entries.length; ++i) {
+		git_tree_entry *entry = bld->entries.contents[i];
+		if (! entry->removed)
+                  ++count;
+	}
+	return count;
 }
 
 static int tree_error(const char *str)
